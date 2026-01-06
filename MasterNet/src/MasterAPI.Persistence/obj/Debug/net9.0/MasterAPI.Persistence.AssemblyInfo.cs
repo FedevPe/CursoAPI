0ea35e54c84aa95a97a7776d84b68711c693187a
Federico <afedeperez2021@gmail.com>
2026-01-06 15:59:38 -0300

    Add Identity.EntityFrameworkCore to Persistence project
    Added the Microsoft.AspNetCore.Identity.EntityFrameworkCore NuGet package to MasterAPI.Persistence.csproj for identity management support. Updated related project and obj files to reflect the new package and project directory changes.

--- MasterNet/src/MasterAPI.Persistence/obj/Debug/net9.0/MasterAPI.Persistence.AssemblyInfo.cs
+++ MasterNet/src/MasterAPI.Persistence/obj/Debug/net9.0/MasterAPI.Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MasterAPI.Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4be5a24e1dd3b7baa60babb86161e2d93726dcb")]
 [assembly: System.Reflection.AssemblyProductAttribute("MasterAPI.Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MasterAPI.Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
